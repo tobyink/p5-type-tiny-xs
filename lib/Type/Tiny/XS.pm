df08cc516c8fbd480b1a77cae10bfc055b21155a
Toby Inkster <mail@tobyinkster.co.uk>
2014-09-06 23:48:35 +0100

    bump version; changelog

--- lib/Type/Tiny/XS.pm
+++ lib/Type/Tiny/XS.pm
@@ -6,7 +6,7 @@ use XSLoader ();
 package Type::Tiny::XS;
 
 our $AUTHORITY = 'cpan:TOBYINK';
-our $VERSION   = '0.010';
+our $VERSION   = '0.011';
 
 __PACKAGE__->XSLoader::load($VERSION);
 
--- xs_version.h
+++ xs_version.h
@@ -1,3 +1,3 @@
 #ifndef XS_VERSION
-#define XS_VERSION "0.010"
+#define XS_VERSION "0.011"
 #endif
